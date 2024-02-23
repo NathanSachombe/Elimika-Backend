cd2a39900e8f0cf1b33b2e63ba2f7384dba8d9e2
kevin <kevinmbari600@gmail.com>
2024-02-23 16:10:17 +0300

    adding email functionality

--- Resources/eventEnrolment.py
+++ Resources/eventEnrolment.py
@@ -1,7 +1,6 @@
-from models import db, UserEventModel, EventModel
+from models import db, UserEventModel, EventModel , UserModel
 from flask_jwt_extended import jwt_required,get_jwt_identity
-from flask_restful import Resource,fields, marshal_with, reqparse,marshal
-
+from flask_restful import Resource,fields,  reqparse,marshal
 
 resource_fields = {
     "id": fields.Integer,
@@ -21,39 +20,4 @@ class EnrolledEvents(Resource):
             return marshal(UserEvent, resource_fields)
         else:
             return {"message":"you have not enrolled for any event"}
-        
-    @jwt_required()
-    def post(self):
-        data = EnrolledEvents.enrolledEvent_parser.parse_args()
-        current_user_id = get_jwt_identity()
-        check_event_existence = EventModel.query.get(data['event_id'])
-
-        if not check_event_existence:
-            return {"message": "Event not found"}, 404
-
-        existing_booking = UserEventModel.query.filter_by(user_id=current_user_id, event_id=data['event_id']).first()
-
-        if existing_booking:
-            # User has already booked for this event
-            return {"message": "You have already booked for this event"}, 400
-        
-        # getting the capacity of the event a person is booking tor so that he cant book if no space
-        event = EventModel.query.get(data['event_id'])
-        if event.capacity <= 0:
-            return {"message": "Event is already fully booked"}, 400
-
-        # Decrementing  event capacity when a user succesfully books an event
-        event.capacity -= 1
-        db.session.commit()
-
-        data['user_id'] = current_user_id
-        new_event = UserEventModel(**data)
-
-        db.session.add(new_event)
-        db.session.commit()
-
-        return {"message": "Event booked successfully"}, 201
-        
-
-
         
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
-from models import db
+from flask_jwt_extended import JWTManager,jwt_required,get_jwt_identity
+from models import db,EventModel,UserEventModel,UserModel
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_cors import CORS
@@ -14,13 +14,27 @@ from Resources.profile import ProfileResource
 from Resources.feedback import Feedback
 from Resources.events import Events
 from Resources.eventEnrolment import EnrolledEvents
+from flask import Flask
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+mail = Mail(app)
 api = Api(app)
 CORS(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
+#email configuration
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'kevin.wanjiru600@gmail.com'
+app.config['MAIL_PASSWORD'] = 'jjpsewfynmpgkeom'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+#database and error handling setup
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BUNDLE_ERRORS'] = True
@@ -40,11 +54,55 @@ api.add_resource(Course, '/course','/course/<int:id>')
 api.add_resource(EnrolledEvents,'/enrolledEvent','/enrolledEvent/<int:id>')
 api.add_resource(Feedback,'/feedback','/feedback/<int:id>')
 api.add_resource(Events,'/event','/event/<int:id>')
-
 api.add_resource(ProfileResource, '/profile','/profile/<int:id>')
-@app.route("/")
-def index():
-    return "<h1>Welcome to Elimika!</h1>"
+
+
+#Function to send a booking confirmation email 
+def send_booking_confirmation_email(email,event_title,username):
+    msg = Message('Event Booking Confirmation', sender='kevin.wanjiru600@example.com', recipients=[email])
+    msg.body = f'Dear {username} You have successfully booked for the event:{event_title} , Hope you enjoy the Event!'
+    mail.send(msg)
+
+@app.route('/enrolledEvent', methods=['POST'])
+@jwt_required()
+def post():
+    data = EnrolledEvents.enrolledEvent_parser.parse_args()
+    current_user_id = get_jwt_identity()
+    check_event_existence = EventModel.query.get(data['event_id'])
+
+    if not check_event_existence:
+        return {"message": "Event not found"}, 404
+
+    existing_booking = UserEventModel.query.filter_by(user_id=current_user_id, event_id=data['event_id']).first()
+
+    if existing_booking:
+        # User has already booked for this event
+        return {"message": "You have already booked for this event"}, 400
+        
+    # getting the capacity of the event a person is booking tor so that he cant book if no space
+    event = EventModel.query.get(data['event_id'])
+    if event.capacity <= 0:
+        return {"message": "Event is already fully booked"}, 400
+
+    # Decrementing  event capacity when a user succesfully books an event
+    event.capacity -= 1
+    db.session.commit()
+
+    data['user_id'] = current_user_id
+    new_event = UserEventModel(**data)
+        
+    db.session.add(new_event)
+    db.session.commit()
+    user = UserModel.query.filter_by(id = current_user_id).first()
+    email = user.email
+    username = user.username
+    event_title = event.title
+    if email and event_title:
+        send_booking_confirmation_email(username,email,event_title )
+    else:
+        return {"message": "Email not found"}, 404
+        
+    return {"message": "Event booked successfully"}, 201
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
